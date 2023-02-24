517b12b95da0d99c3936fce2cd24b54af060db9e
Xavier Saint-Mleux <xavier.saint-mleux@naisense.ai>
2023-02-24 14:52:34 -0500

    wrong include

--- Source/ROSIntegration/Private/viz/ROSBridgeConnection.cpp
+++ Source/ROSIntegration/Private/viz/ROSBridgeConnection.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "ROSIntegrationGameInstance.h"
+#include "viz/ROSBridgeConnection.h"
 
 
 AROSBridgeConnection::AROSBridgeConnection()
